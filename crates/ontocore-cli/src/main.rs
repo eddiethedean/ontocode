ca07e6ad7ee933c05a3b06c030bff9e127822a3d
Odos Matthews <odosmatthews@gmail.com>
2026-07-13 21:21:26 -0400

    fix(cli): borrow PathBuf for paths_refer_to_same.

--- crates/ontocore-cli/src/main.rs
+++ crates/ontocore-cli/src/main.rs
@@ -457,7 +457,7 @@ fn main() -> Result<()> {
                 .data()
                 .documents
                 .iter()
-                .find(|d| ontocore_core::paths_refer_to_same(&d.path, document))
+                .find(|d| ontocore_core::paths_refer_to_same(&d.path, &document))
                 .map(|d| d.namespaces.clone())
                 .unwrap_or_default();
             if ext == "obo" {
